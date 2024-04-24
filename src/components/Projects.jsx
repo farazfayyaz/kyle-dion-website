b1b236e01ef8ce1aeabc8e77e4369d6afe1da64d
farazfayyaz <farazfayyazwork@gmail.com>
2024-04-24 17:56:05 -0400

    updating icon sub text and spacing

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,59 +13,59 @@ function Projects() {
 
         <div className='w-full my-5 flex gap-5'>
 
-            <div className='flex flex-col py-6 gap-4 items-center outline outline-1'>
+            <div className='flex flex-col py-6 gap-4 items-center rounded-2xl outline outline-1'>
                 <img src={sassyImg} className='w-1/2 rounded-2xl'></img>
                 <h2 className='text-3xl font-bold'>SASSY</h2>
-                <span className='flex justify-center gap-8 outline outline-1'>
-                    <button className='flex flex-col items-center'>
+                <span className='flex justify-center gap-8 p-6 outline outline-1'>
+                    <button className='flex flex-col items-center gap-3'>
                         <img src={spotify} className='w-12 h-12'></img>
-                        <p>Spotify</p>
+                        <p className='text-lg'>Spotify</p>
                     </button>
-                    <button className='flex flex-col items-center'>
+                    <button className='flex flex-col items-center gap-3'>
                         <img src={apple} className='w-12 h-12'></img>
-                        <p>Apple Music</p>
+                        <p className='text-lg'>Apple Music</p>
                     </button>
-                    <button className='flex flex-col items-center'>
+                    <button className='flex flex-col items-center gap-3'>
                         <img src={youtube} className='w-12 h-12'></img>
-                        <p>YouTube</p>
+                        <p className='text-lg'>YouTube</p>
                     </button>
                 </span>
             </div>
 
-            <div className='flex flex-col py-6 gap-4 items-center outline outline-1'>
+            <div className='flex flex-col py-6 gap-4 items-center rounded-2xl outline outline-1'>
                 <img src={sugaImg} className='w-1/2 rounded-2xl'></img>
                 <h2 className='text-3xl font-bold'>SUGA</h2>
-                <span className='flex justify-center gap-5 outline outline-1'>
-                    <button className='flex flex-col items-center'>
+                <span className='flex justify-center gap-8 p-6 outline outline-1'>
+                    <button className='flex flex-col items-center gap-3'>
                         <img src={spotify} className='w-12 h-12'></img>
-                        <p>Spotify</p>
+                        <p className='text-lg'>Spotify</p>
                     </button>
-                    <button className='flex flex-col items-center'>
+                    <button className='flex flex-col items-center gap-3'>
                         <img src={apple} className='w-12 h-12'></img>
-                        <p>Apple Music</p>
+                        <p className='text-lg'>Apple Music</p>
                     </button>
-                    <button className='flex flex-col items-center'>
+                    <button className='flex flex-col items-center gap-3'>
                         <img src={youtube} className='w-12 h-12'></img>
-                        <p>YouTube</p>
+                        <p className='text-lg'>YouTube</p>
                     </button>
                 </span>
             </div>
 
-            <div className='flex flex-col py-6 gap-4 items-center outline outline-1'>
+            <div className='flex flex-col py-6 gap-4 items-center rounded-2xl outline outline-1'>
                 <img src={paintingSoundsImg} className='w-1/2 rounded-2xl'></img>
                 <h2 className='text-3xl font-bold'>Painting Sounds</h2>
-                <span className='flex justify-center gap-5 outline outline-1'>
-                    <button className='flex flex-col items-center'>
+                <span className='flex justify-center gap-8 p-6 outline outline-1'>
+                    <button className='flex flex-col items-center gap-3'>
                         <img src={spotify} className='w-12 h-12'></img>
-                        <p>Spotify</p>
+                        <p className='text-lg'>Spotify</p>
                     </button>
-                    <button className='flex flex-col items-center'>
+                    <button className='flex flex-col items-center gap-3'>
                         <img src={apple} className='w-12 h-12'></img>
-                        <p>Apple Music</p>
+                        <p className='text-lg'>Apple Music</p>
                     </button>
-                    <button className='flex flex-col items-center'>
+                    <button className='flex flex-col items-center gap-3'>
                         <img src={youtube} className='w-12 h-12'></img>
-                        <p>YouTube</p>
+                        <p className='text-lg'>YouTube</p>
                     </button>
                 </span>
             </div>
