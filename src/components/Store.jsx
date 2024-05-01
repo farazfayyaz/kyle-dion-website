ec9eed2aa6caad257d4ba153202bd7e94432ad88
farazfayyaz <farazfayyazwork@gmail.com>
2024-04-30 20:46:12 -0400

    updating store section outline

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 function Store() {
   return (
     <div className='w-full px-16 my-28 flex justify-center outline outline-4 outline-purple-600'>
-        <div className='flex-col text-center'>
-            <h1 className='flex justify-center'>Store</h1>
+        <div className='w-1/3 flex flex-col items-center'>
+            <h1 className='w-full flex justify-center'>Store</h1>
 
             <h3>Coming Soon</h3>
 
-            <div className='flex-col'>
+            <div className='flex flex-col items-center outline outline-1'>
                 <label for='email'>Sign Up For Email Alerts!</label>
                 <input type='email' id='email' name='email'
                 className='outline outline-1 rounded'></input>
