8e9c26e23a8443e08579a03464d6e38e81e590ea
farazfayyaz <farazfayyazwork@gmail.com>
2024-05-02 20:45:54 -0400

    updating store section outline

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -6,14 +6,17 @@ function Store() {
         
         <h1 className='w-full flex justify-center'>Store</h1>
         
-        <div className='w-1/4 py-10 gap-6 flex flex-col items-center outline outline-1'>
+        <div className='w-2/5 py-10 gap-6 flex flex-col items-center rounded-2xl outline outline-1'>
 
-            <h3 className='text-2xl'>Coming Soon</h3>
+            <h3 className='text-3xl tracking-wide'>Coming Soon</h3>
 
             <div className='flex flex-col gap-6 items-center'>
                 <label for='email'>Sign Up For Email Alerts!</label>
                 <input type='email' id='email' name='email' placeholder='youremail@email.com'
-                className='outline outline-1 rounded'></input>
+                className='h-10 w-64  outline outline-1 rounded'></input>
+                <button
+                  className='h-12 w-52 rounded-full outline outline-1'
+                >Keep Me Updated</button>
             </div>
         </div>
         
