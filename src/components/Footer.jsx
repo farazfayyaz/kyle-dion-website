5a18d0e38667bacfba07e83bebe36a2e98f4d143
farazfayyaz <farazfayyazwork@gmail.com>
2024-05-07 19:54:55 -0400

    updating footer disclaimer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className='flex justify-center text-center outline outline-4 outline-orange-600'>
-        <h4>Disclaimer:</h4>
-        <p>This is a creative project inspired by <a href='https://www.kyledion.co/'>Kyle Dion</a> All credit goes to Kyle Dion and his team.</p>
+    <footer className='h-12 flex justify-center text-center bg-blue-600'>
+        <h4 className='bg-transparent font-bold'>Disclaimer:</h4>
+        <p className='bg-transparent'>This is a creative project inspired by <a className='bg-transparent font-bold underline underline-offset-4' href='https://www.kyledion.co/'>Kyle Dion</a> All credit goes to Kyle Dion and his team.</p>
     </footer>
   )
 }
