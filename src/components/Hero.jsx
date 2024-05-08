2e2fd8d97b9859bd8c51b0e5637325671f669d5a
farazfayyaz <farazfayyazwork@gmail.com>
2024-05-07 20:27:25 -0400

    adding hero section button functionality

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,11 +9,15 @@ function Hero() {
             <div className='flex flex-col h-1/2 w-1/4 gap-10 items-center text-center'>
                 <h1>'If My Jeans Could Talk' Out Now</h1>
                 <p className='text-2xl'>Available On All Platforms.</p>
-                <button 
+                <a
+                href='https://presave.io/t/ifmyjeanscouldtalk?fbclid=PAAaarBwv032yZklRJGdB0dUc5MbYY1TJuETVqv57bRTtaWvRp-DIfkwweQfU_aem_AaYVeiQI9svpJTzLGEx8pHN1G6tUXqDM5L11s5XRPXmMIvSzD058FsiDL41r-7JZbAg'
                 className='h-12 w-36 text-lg tracking-wide font-bold self-center rounded-full border-2 border-white hover:bg-amber-500 transition ease-in-out delay-75 duration-300'
                 >
-                    Listen Now
-                </button>
+                    <button className='h-full w-full bg-transparent'>
+                        Listen Now
+                    </button>
+                    
+                </a>
             </div>
 
             <div className='h-1/2 w-1/3 my-8 rounded-2xl'>
