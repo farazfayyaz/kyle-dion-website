d75983fb155300eb88d8a01545679e83dfc33fd1
farazfayyaz <farazfayyazwork@gmail.com>
2024-05-26 21:10:08 -0400

    updating navbar on mobile screens

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Nav() {
   return (
-    <header className='w-full h-18'>
+    <header className='w-full py-18'>
             <nav>
-                <ul className='m-12 flex justify-center gap-9 text-center text-2xl font-bold'>
+                <ul className='m-12 flex justify-center gap-9 text-center text-2xl font-bold max-lg:gap-3 text-lg'>
                     <li><a href='#about' className='hover:text-amber-500 transition ease-in-out delay-75 duration-300 cursor-pointer'>About</a></li>
                     <li><a href='#projects' className='hover:text-amber-500 transition ease-in-out delay-75 duration-300 cursor-pointer'>Projects</a></li>
                     <li><a href='#shows' className='hover:text-amber-500 transition ease-in-out delay-75 duration-300 cursor-pointer'>Shows</a></li>
